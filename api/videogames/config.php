49ce2441d9d7977e2ce29761776502a93e8f6fb5
DavidIncarbone <david.incarbone@gmail.com>
2025-07-04 15:38:25 +0200

    install vlucas/phpdotenv for load env variables

--- api/videogames/config.php
+++ api/videogames/config.php
@@ -1,11 +1,17 @@
 <?php
 
+require_once __DIR__ . '/../../vendor/autoload.php';
 
+$envPath = __DIR__ . '/../../';
 
-$host = 'localhost';
-$dbname = 'db_videogames';
-$user = 'root';
-$password = 'Dekithedragon.91';
+$dotenv = Dotenv\Dotenv::createImmutable($envPath);
+$dotenv->load();
+
+$user = $_ENV['DB_USER'];
+$host =  $_ENV['DB_HOST'];
+$password = $_ENV['DB_PASS'];
+$dbname = $_ENV['DB_NAME'];
+
 
 try {
 
--- api/videogames/functions.php
+++ api/videogames/functions.php
@@ -30,3 +30,16 @@ function generateUniqueSlug(PDO $pdo, string $table, string $column, string $nam
 
     return $slug;
 }
+
+function loadEnv($path)
+{
+    if (!file_exists($path)) return;
+
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (str_starts_with(trim($line), '#') || !str_contains($line, '=')) continue;
+
+        list($key, $value) = explode('=', $line, 2);
+        putenv(trim($key) . '=' . trim($value));
+    }
+}
